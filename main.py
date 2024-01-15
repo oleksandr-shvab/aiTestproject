a2be77f4cf1fa398bef08b63d6d38b25a3e122d2
alexandr <80922850741@ukr.net>
2024-01-15 11:58:47 +0200

    cirrhisis

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import os
 import joblib
 import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 
 def make_prediction():
@@ -33,24 +36,45 @@ def train_model():
     """
     Train Logistic Regression model and save it
     """
-    train_file_path = 'train.csv'
+    # Load your dataset
+    data = pd.read_csv('cirrhosis.csv')
 
-    train_data = pd.read_csv(train_file_path, encoding='ISO-8859-1')
+    # Handling missing values
+    data.fillna(method='ffill', inplace=True)
 
-    # Data Preprocessing
-    train_data['text'] = train_data['text'].fillna('')
-    train_data['sentiment'] = train_data['sentiment'].astype(str)
+    # Separate features and target variable
+    X = data.drop('Stage', axis=1)
+    y = data['Stage']
 
-    tfidf_vectorizer = TfidfVectorizer(max_features=5000)
-    x_train = tfidf_vectorizer.fit_transform(train_data['text'])
-    y_train = train_data['sentiment']
+    # Categorical and numerical column names
+    categorical_cols = X.select_dtypes(include=['object']).columns
+    numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
 
-    # Model Training
-    model = LogisticRegression(max_iter=1000)
-    model.fit(x_train, y_train)
+    # Create a ColumnTransformer for preprocessing
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', StandardScaler(), numerical_cols),
+            ('cat', OneHotEncoder(), categorical_cols)
+        ])
 
-    joblib.dump(model, 'sentiment_model.pkl')
-    joblib.dump(tfidf_vectorizer, 'tfidf_vectorizer.pkl')
+    # Create preprocessing and training pipeline
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                               ('model', LogisticRegression())])
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Train the model
+    model = pipeline.fit(X_train, y_train)
+
+    joblib.dump(model, "cirrhosis_model.pkl")
+
+    y_pred = model.predict(X_test)
+
+    print(y_pred)
+
+    # Print the classification report
+    print(classification_report(y_test, y_pred))
 
 
 def main_menu():
